da273eeac0027500af230b8ee5b8edd40e51bf9c
Seung Hun Han <shhan730@gmail.com>
2019-08-10 04:52:42 +0900

    Add active button method

--- components/Header.vue
+++ components/Header.vue
@@ -19,9 +19,11 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn icon to="/account" nuxt>
-      <v-icon>mdi-account-circle</v-icon>
-    </v-btn>
+      <v-btn icon v-if="true" to="/account" nuxt>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+      <v-btn v-else style="margin-right:40px;">Login</v-btn>
+
   </v-app-bar>
 </template>
 
