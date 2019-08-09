ad16c1cd9ef93dffa5429cf69ce7700c4fc88d56
PSJ <astro.psj@gmail.com>
2019-08-10 04:19:00 +0900

    Lint codes

--- pages/account.vue
+++ pages/account.vue
@@ -2,104 +2,80 @@
   <v-container>
     <v-layout column justify-center align-center>
       <v-flex xs12 sm8 md6>
-
         <div id="title">
-          <strong >
+          <strong>
             <v-icon id="icon_account">mdi-account</v-icon>
             사용자 설정
           </strong>
         </div>
 
         <div class="infoCard">
-          <v-card
-            width="750px"
-            class="mx-auto"
-          >
-          <v-card-title class="card_name">이름</v-card-title>
+          <v-card width="750px" class="mx-auto">
+            <v-card-title class="card_name">이름</v-card-title>
             <v-card-text class="card_info">(사용자 이름)</v-card-text>
           </v-card>
         </div>
 
         <div class="infoCard">
-          <v-card
-            width="750px"
-            class="mx-auto"
-          >
-          <v-card-title class="card_name">사용자 ID</v-card-title>
+          <v-card width="750px" class="mx-auto">
+            <v-card-title class="card_name">사용자 ID</v-card-title>
             <v-card-text class="card_info">(사용자 ID)</v-card-text>
           </v-card>
         </div>
 
         <div class="infoCard">
-          <v-card
-            width="750px"
-            class="mx-auto"
-          >
-          <v-card-title class="card_name">사용자 부서</v-card-title>
+          <v-card width="750px" class="mx-auto">
+            <v-card-title class="card_name">사용자 부서</v-card-title>
             <v-card-text class="card_info">(사용자 부서)</v-card-text>
           </v-card>
         </div>
 
         <div class="infoCard">
-          <v-card
-            width="750px"
-            class="mx-auto"
-          >
-          <v-card-title class="card_name">사용자 직책</v-card-title>
+          <v-card width="750px" class="mx-auto">
+            <v-card-title class="card_name">사용자 직책</v-card-title>
             <v-card-text class="card_info">(사용자 직책)</v-card-text>
           </v-card>
         </div>
 
         <div class="infoCard">
-          <v-card
-            width="750px"
-            class="mx-auto"
-          >
+          <v-card width="750px" class="mx-auto">
             <v-icon id="icon_key">mdi-account-key</v-icon>
-            <v-btn text id="btn_password" to="/pswChange" nuxt>비밀번호 변경</v-btn>
+            <v-btn id="btn_password" text to="/pswChange" nuxt
+              >비밀번호 변경</v-btn
+            >
           </v-card>
         </div>
-
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+export default {}
 </script>
 
 <style>
-  #title{
-    margin-top: 10px;
-    margin-bottom: 20px;
-    font-weight:bold;
-    font-size: 30px;
-  }
-  #btn_password{
-    font-size: 15px;
-    font-weight:bold;
-  }
-  #icon_key{
-    padding-left: 10px 
-  }
-  #icon_account{
-    font-size: 40px
-  }
-  .card_name{
-    font-size: 20px;
-    font-weight:bold;
-  }
-  .infoCard{
-    margin-top: 20px;
-  }
+#title {
+  margin-top: 10px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 30px;
+}
+#btn_password {
+  font-size: 15px;
+  font-weight: bold;
+}
+#icon_key {
+  padding-left: 10px;
+}
+#icon_account {
+  font-size: 40px;
+}
+.card_name {
+  font-size: 20px;
+  font-weight: bold;
+}
+.infoCard {
+  margin-top: 20px;
+}
 </style>
-
--- pages/pswChange.vue
+++ pages/pswChange.vue
@@ -1,105 +1,97 @@
 <template>
   <v-form>
     <v-container>
-
       <v-layout wrap justify-center>
         <v-flex class="text-center">
-
           <div id="title">
-              <strong >
-                <v-icon id="icon_key">mdi-account-key</v-icon>
-                비밀번호 변경
-              </strong>
-            </div>
-          
-          <div class="infoCard">
-              <v-card
-                width="750px"
-                class="mx-auto"
-              ></v-card>
+            <strong>
+              <v-icon id="icon_key">mdi-account-key</v-icon>
+              비밀번호 변경
+            </strong>
           </div>
 
+          <div class="infoCard">
+            <v-card width="750px" class="mx-auto"></v-card>
+          </div>
         </v-flex>
-          <v-flex xs12 sm6 class="psw">
-            <v-text-field
-              v-model="password"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              label="기존 비밀번호를 입력하세요."
-              hint="기존 비밀번호"
-              counter
-              class="psw_text"
-            ></v-text-field>
+        <v-flex xs12 sm6 class="psw">
+          <v-text-field
+            v-model="password"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="기존 비밀번호를 입력하세요."
+            hint="기존 비밀번호"
+            counter
+            class="psw_text"
+          ></v-text-field>
 
-            <v-text-field
-              v-model="password"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              label="신규 비밀번호를 입력하세요."
-              hint="신규 비밀번호"
-              counter
-              class="psw_text"
-            ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="신규 비밀번호를 입력하세요."
+            hint="신규 비밀번호"
+            counter
+            class="psw_text"
+          ></v-text-field>
 
-            <v-text-field
-              v-model="password"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              label="신규 비밀번호를 다시 입력하세요."
-              hint="신규 비밀번호 재입력"
-              counter
-              class="psw_text"
-            ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="신규 비밀번호를 다시 입력하세요."
+            hint="신규 비밀번호 재입력"
+            counter
+            class="psw_text"
+          ></v-text-field>
 
-            <v-btn block color="secondary" dark>비밀번호 변경</v-btn>
-
+          <v-btn block color="secondary" dark>비밀번호 변경</v-btn>
         </v-flex>
-
       </v-layout>
     </v-container>
   </v-form>
 </template>
 
 <style>
-  #icon_key{
-    font-size: 40px;
-  }
-  #title{
-    padding-top: 30px;
-  }
-  .card_psw{
-    margin-left: 20px;
-    padding: 0px;
-  }
-  .textField{
-    transform: scale(1.0);
-  }
-  .psw{
-    margin-top: 50px;
-  }
-  .psw_text{
-    margin-bottom: 40px;
-  }
+#icon_key {
+  font-size: 40px;
+}
+#title {
+  padding-top: 30px;
+}
+.card_psw {
+  margin-left: 20px;
+  padding: 0px;
+}
+.textField {
+  transform: scale(1);
+}
+.psw {
+  margin-top: 50px;
+}
+.psw_text {
+  margin-bottom: 40px;
+}
 </style>
 
 <script>
-  export default {
-    data () {
-      return {
-        show1: true,
-        show2: true,
-        show3: false,
-        show4: false,
-        password: '',
-        rules: {
-          required: value => !!value || 'Required.',
-          //min: v => v.length >= 8 || 'Min 8 characters',
-          //emailMatch: () => ('The email and password you entered don\'t match'),
-        },
+export default {
+  data() {
+    return {
+      show1: true,
+      show2: true,
+      show3: false,
+      show4: false,
+      password: '',
+      rules: {
+        required: (value) => !!value || 'Required.'
+        // min: v => v.length >= 8 || 'Min 8 characters',
+        // emailMatch: () => ('The email and password you entered don\'t match'),
       }
-    },
+    }
   }
+}
 </script>
